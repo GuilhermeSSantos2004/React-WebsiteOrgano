6b73055ff0500e7cf154b96cc60c30b2f5ea24eb
guilherme <guilhermesilvas2004@gmail.com>
2024-03-24 19:49:24 -0300

    Recebendo props func

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,13 +2,16 @@ import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
-    //console.log(props)    
+   console.log(props)    
+   console.log(props.placeholder)
+
+   const placeholderModificada = ` ${props.placeholder}...`;
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input placeholder={props.placeholder}/>
+            <input placeholder={props.placeholder} />
         </div>
     )
 }
