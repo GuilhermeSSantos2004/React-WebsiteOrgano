d6b2807243872741600eace8386d2fcc7d01859e
guilherme <guilhermesilvas2004@gmail.com>
2024-04-11 23:39:35 -0300

    add card

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ function App() {
       corPrimaria: '#FF8A29',
       corSecundaria: '#FFEEDF',
     }
+    
   
 ]
 
@@ -56,10 +57,15 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
       
-      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria}
-      corSecundaria={time.corSecundaria}/>)}
+      {times.map(time => <Time 
+      key={time.nome} 
+      nome={time.nome} 
+      corPrimaria={time.corPrimaria}
+      corSecundaria={time.corSecundaria}
+      colaboradores={colaboradores}
+      />)}
       
     </div>
   );
